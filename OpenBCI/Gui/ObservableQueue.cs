3430a79f941a2c53cd9798e14ab118d0683877d9
DanglingPointer <mikail.vasilyev@gmail.com>
2017-11-06 13:37:13 +0100

    Gui looks nice

---
+++ OpenBCI/Gui/ObservableQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui
+{
+    public class ObservableQueue<T> : Queue<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableQueue() : base()
+        { }
+        public ObservableQueue(IEnumerable<T> collection) : base(collection)
+        { }
+        public ObservableQueue(int capacity) : base(capacity)
+        { }
+        public new T Dequeue()
+        {
+            T removed = base.Dequeue();
+            OnCollectionChanged(NotifyCollectionChangedAction.Remove, removed, 0);
+            OnCountChanged();
+            return removed;
+        }
+        public new void Enqueue(T item)
+        {
+            int index = Count;
+            base.Enqueue(item);
+            OnCollectionChanged(NotifyCollectionChangedAction.Add, item, index);
+            OnCountChanged();
+        }
+        public new void Clear()
+        {
+            base.Clear();
+            OnCollectionReset();
+            OnCountChanged();
+        }
+
+        private void OnCollectionChanged(NotifyCollectionChangedAction action, object item, int index)
+        {
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(action, item, index));
+        }
+        private void OnCollectionReset()
+        {
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+        private void OnCountChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
+        }
+    }
+}
---
+++ OpenBCI/Gui/ViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.DataVisualization;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Input;
+
+namespace Gui
+{
+    /// <summary>
+    /// Helper class for MVVM
+    /// </summary>
+    class RelayCommand : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+            _canExecute = (o) => true;
+        }
+        public void Execute(object o)
+        {
+            _execute(o);
+        }
+        public bool CanExecute(object o)
+        {
+            return _canExecute(o);
+        }
+        public void ExecuteChanged(object sender, EventArgs e)
+        {
+            CanExecuteChanged?.Invoke(sender, e);
+        }
+    }
+
+    /// <summary>
+    /// DataContext for Main window
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private const int DATA_SERIES_LENGTH = 100;
+        private long _currentX;
+        private readonly Random _rand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            _rand = new Random();
+            _currentX = 0;
+            DataSeries = new ObservableQueue<KeyValuePair<long, double>>(DATA_SERIES_LENGTH);
+
+            for (int i = 0; i < DATA_SERIES_LENGTH; ++i) {
+                DataSeries.Enqueue(new KeyValuePair<long, double>(_currentX++, _rand.NextDouble()));
+            }
+            OnStartButtonPressed = new RelayCommand((o) => {
+                DataSeries.Dequeue();
+                DataSeries.Enqueue(new KeyValuePair<long, double>(_currentX++, _rand.NextDouble()));
+                OnPropertyChanged(nameof(DataSeries)); // unnecessary
+            });
+            OnStopButtonPressed = new RelayCommand((o) => {
+                InfoText += "yo";
+                OnPropertyChanged(nameof(InfoText));
+            });
+        }
+        public ICommand OnStartButtonPressed
+        { get; }
+        public ICommand OnStopButtonPressed
+        { get; }
+        public ObservableQueue<KeyValuePair<long, double>> DataSeries
+        { get; }
+        public string InfoText
+        { get; private set; }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
